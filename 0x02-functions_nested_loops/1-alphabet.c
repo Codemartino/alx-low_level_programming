a192a1bc10dc8f6801a1ffe415ee5428dcd0d3d4
Codemartino <akudugumartin5@gmail.com.com>
2023-03-21 13:29:40 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alpahbet in lowercase.
+ * Return: Always 0 (success)
+ */
+void print_alpahbet(void)
+{
+	int i;
+
+	for (i = 97; i<= 122; i++)
+	{
+		-putchar(i)
+	}
+	_putchar('\n')
+         
+
+
